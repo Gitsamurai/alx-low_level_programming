e5199eff51b59ec7fcd3708578fc19905b04bd31
Isaac <oansah>
2022-04-11 03:27:22 -0700

    C language in progress

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 /**
-*main - entry point
-*
-*Return: 0 after executing the function return 0
-*/
+ *main - entry point
+ *
+ *Return: 0 after printing the function 
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
+} 
