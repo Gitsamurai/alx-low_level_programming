97b7adbc05473dd9fbe9d27b66228aa34a37dac1
Isaac <oansah>
2022-04-14 15:28:27 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+#follow bett comment style 
+
+int main(void)
+{
+int c;
+for  (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c <26; ++c)
+putchar('A' +c);
+
+putchar('\n');
+return (0);
+}
