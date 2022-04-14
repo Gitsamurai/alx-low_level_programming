3d91932880d5dd0031df37bb52b8361508b6f207
Isaac <oansah>
2022-04-14 15:35:38 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
-#follow bett comment style 
-
+/**
+ *main - entry point
+ *
+ *Description: print lower and Upper alphabet on the same line
+ *Return: 0
+ */
 int main(void)
 {
 int c;
-for  (c = 0; c < 26; ++c)
+
+for (c = 0; c < 26; ++c)
 putchar('a' + c);
 for (c = 0; c <26; ++c)
 putchar('A' +c);
 
 putchar('\n');
+
 return (0);
 }
