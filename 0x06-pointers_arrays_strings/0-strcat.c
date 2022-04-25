250c504acc92cd1af7e071f06938f6f3fdd4218c
Isaac <oansah>
2022-04-25 14:55:59 -0700

    REdid task1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,16 @@
+/**
+ * _strcat - function that concatenates two strings.
+ * @dest: pointer to destination input
+ * @src: pointerto resulting string @dest
+ * */
+
 char *_strcat(char *dest, char *src)
 {
 int c, c2;
-c=0;
-/*find the size of dest array*/
+c = 0;
 while (dest[c])
 c++;
-/*iterate through each src array value without the null byte*/
-for (c2 = 0; src[c2] ; c2++)
-/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+for (c2 =0; src[2] ; c2++)
 dest[c++] = src[c2];
 return (dest);
-} 
+}
